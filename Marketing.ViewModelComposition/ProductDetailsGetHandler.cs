47b89400b75a8e5e2d419bd6638406d2bc424a8a
Mauro Servienti <mauro.servienti@gmail.com>
2017-11-30 08:19:22 +0100

    Add Marketing composition skeleton

---
+++ Marketing.ViewModelComposition/ProductDetailsGetHandler.cs
@@ -0,0 +1,40 @@
+﻿using ITOps.ViewModelComposition;
+using ITOps.ViewModelComposition.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Marketing.ViewModelComposition
+{
+    public class ProductDetailsGetHandler : IHandleRequests
+    {
+        public bool Matches(RouteData routeData, string httpVerb, HttpRequest request)
+        {
+            //determine if the incoming request should 
+            //be composed with Marketing data, e.g.
+            var controller = (string)routeData.Values["controller"];
+            var action = (string)routeData.Values["action"];
+
+            return HttpMethods.IsGet(httpVerb)
+                && controller.ToLowerInvariant() == "products"
+                && action.ToLowerInvariant()== "details"
+                && routeData.Values.ContainsKey("id");
+        }
+
+        public async Task Handle(dynamic vm, RouteData routeData, HttpRequest request)
+        {
+            //invoke Marketing back-end API to retrieve marketing related product details
+            var id = (string)routeData.Values["id"];
+
+            var url = $"http://localhost:....../....../{id}";
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+
+            dynamic productDetails = await response.Content.AsExpandoAsync();
+
+            vm.ProductName = productDetails.Name;
+            vm.ProductDescription = productDetails.Description;
+        }
+    }
+}
